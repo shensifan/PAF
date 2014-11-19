9224cf0db298bef22e789c5f1f72c2152b8f37fa
sifan <shensifan2004@163.com>
2014-11-19 18:07:20 +0800

    修改配置文件模板

--- PAF/config_tmplate.py
+++ PAF/config_tmplate.py
@@ -7,9 +7,9 @@
 #
 ################################################################################
 """
-    Author  :   dingyingcai(dingyingcai@baidu.com)
+    Author  :   shensifan(shensifan2004@163.com)
     Date    :   2014/11/06 14:28:00
-    Desc    :   template config for PAFServer
+    Desc    :   PAFServer配置文件模板
 """
 ################服务端配置###########################
 #命名空间,用于区分部门或者分组
@@ -22,12 +22,16 @@ SERVICE = "stat"
 GROUP = None
 #工作线程数量
 WORKER_COUNT = 20
-#队列长度
-MAX_Q_SIZE = 10000000
+#队列长度,加入过载保护,当队列过长时丢包
+#QUEUE_SIZE = 10000000
+#队列超时时间,在队列中呆时间过长后丢包
+#QUEUE_TIME_OUT
 #服务端口
 LISTEN_IP = "127.0.0.1"
 LISTEN_PORT = 10000
-#远程日志服务器地址
+#本地日志路径
+LOG_PATH = None
+#远程日志服务器
 LOG_SERVER = None
 #node服务器地址,永远是127.0.0.1
 NODE_SERVER = ('127.0.0.1', 9999)
@@ -35,9 +39,9 @@ NODE_SERVER = ('127.0.0.1', 9999)
 ################客户端配置###########################
 #回调线程数量
 CALLBACK_COUNT = 20
-#位置服务器地址,用于注册和查询服务
+#位置服务器地址,用于查询其它服务位置
 LOCATER_SERVER = ('127.0.0.1', 8001)
 
 ################公用配置##########################
-#统计服务器
+#调用统计服务
 STAT_SERVER = "EE.PAF.stat"
